ff5c04f234ea9c886d7f3dcba8ab29f90be1d9c7
laxamore <mukhliskurnia60@gmail.com>
2019-01-24 03:01:44 +0700

    update

--- mpu6050.py
+++ mpu6050.py
@@ -1,59 +1,59 @@
-#!/usr/bin/python3
-
-import smbus
 import math
+import smbus
 
-def read_word(reg):
-    h = bus.read_byte_data(address, reg)
-    l = bus.read_byte_data(address, reg+1)
-    value = (h << 8) + l
-    return value
+class mpu6050:
+    address_mpu = 0x68
+    power_mgmt_1 = 0x6b
+    i2c_bypass_address = 0x37
 
-def read_word_2c(reg):
-    val = read_word(reg)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
+    def __init__(self, port):
+        self.bus = smbus.SMBus(port)
+        self.bus.write_byte_data(self.address_mpu, self.power_mgmt_1, 0)
+        self.bus.write_byte_data(self.address_mpu, self.i2c_bypass_address, 2)
 
-bus = smbus.SMBus(1)
-address = 0x68
-power_mgmt_1 = 0x6b
 
-bus.write_byte_data(address, power_mgmt_1, 0)
+    def read_word(self, reg):
+        h = self.bus.read_byte_data(self.address_mpu, reg)
+        l = self.bus.read_byte_data(self.address_mpu, reg+1)
+        value = (h << 8) + l
+        return value
 
-pitch = 0
-roll = 0
+    def read_word_2c(self, reg):
+        val = self.read_word(reg)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
 
-def ComplementaryFilter(accData, gyroData, p, r):
-    dt = 0.01 #Sample Time 10ms
-    ACCELEROMETER_SENSITIVITY = 8192.0
-    GYROSCOPE_SENSITIVITY = 65.536
+    def ComplementaryFilter(self, accData, gyroData, p, r):
+        dt = 0.01 #Sample Time 10ms
+        ACCELEROMETER_SENSITIVITY = 8192.0
+        GYROSCOPE_SENSITIVITY = 65.536
 
-    pitchAcc = 0.0
-    rollAcc = 0.0
-    
-    p += (gyroData[0] / GYROSCOPE_SENSITIVITY) * dt
-    r -= (gyroData[1] / GYROSCOPE_SENSITIVITY) * dt
-    
-    forceMagnitudeApprox = abs(accData[0]) + abs(accData[1]) + abs(accData[2])
-    if forceMagnitudeApprox > 8192 and forceMagnitudeApprox < 32768:
-        pitchAcc = math.atan2(float(accData[1]), float(accData[2])) * 180 / math.pi;
-        p = p * 0.98 + pitchAcc * 0.02;
-        rollAcc = math.atan2(float(accData[0]), float(accData[2])) * 180 / math.pi;
-        r = r * 0.98 + rollAcc * 0.02;
-    
-    return p, r
+        pitchAcc = 0.0
+        rollAcc = 0.0
+        
+        p += (gyroData[0] / GYROSCOPE_SENSITIVITY) * dt
+        r -= (gyroData[1] / GYROSCOPE_SENSITIVITY) * dt
+        
+        forceMagnitudeApprox = abs(accData[0]) + abs(accData[1]) + abs(accData[2])
+        if forceMagnitudeApprox > 8192 and forceMagnitudeApprox < 32768:
+            pitchAcc = math.atan2(float(accData[1]), float(accData[2])) * 180 / math.pi;
+            p = p * 0.98 + pitchAcc * 0.02;
+            rollAcc = math.atan2(float(accData[0]), float(accData[2])) * 180 / math.pi;
+            r = r * 0.98 + rollAcc * 0.02;
+        
+        return p, r
 
-while True:
-    acceleration_xout = read_word_2c(0x3b)
-    acceleration_yout = read_word_2c(0x3d)
-    acceleration_zout = read_word_2c(0x3f)
-    
-    gyro_xout = read_word_2c(0x43)
-    gyro_yout = read_word_2c(0x45)
-    gyro_zout = read_word_2c(0x47)
+    def get_pitch_roll(self, pitch, roll):
+        acceleration_xout = self.read_word_2c(0x3b)
+        acceleration_yout = self.read_word_2c(0x3d)
+        acceleration_zout = self.read_word_2c(0x3f)
+        
+        gyro_xout = self.read_word_2c(0x43)
+        gyro_yout = self.read_word_2c(0x45)
+        gyro_zout = self.read_word_2c(0x47)
 
-    pitch, roll = ComplementaryFilter([acceleration_xout, acceleration_yout, acceleration_zout], [gyro_xout, gyro_yout, gyro_zout], pitch, roll)
+        pitch, roll = self.ComplementaryFilter([acceleration_xout, acceleration_yout, acceleration_zout], [gyro_xout, gyro_yout, gyro_zout], pitch, roll)
 
-    print(pitch, roll)
+        return pitch, roll
